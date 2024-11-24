6936028d9ccc7f11602eae583fe521a426f8d305
josh <>
2024-11-23 19:23:42 -0800

    add tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,17 +1,19 @@
 use crate::token::Token;
 
 pub struct Scanner {
-    source: String
+    source: String,
+    tokens: Vec<Token>
 }
 
 impl Scanner {
     pub fn new(source: &str) -> Self {
         Scanner {
-            source: source.to_string()
+            source: source.to_string(),
+            tokens: Vec::new() // On init
         }
     }
 
     pub fn scan_tokens(&self) -> Vec<Token> {
-        Vec::new()
+        Vec::new() // TODO
     }
 }
